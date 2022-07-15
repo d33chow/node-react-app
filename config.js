943dc1e6029272ea076526d9ef5867822517ebad
Dylan Chow <72838930+d33chow@users.noreply.github.com>
2022-07-15 10:39:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
+  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3092',
   user    : 'd33chow',
   password: 'auth_string',
   database: 'd33chow'
